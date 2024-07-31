7c0536d8b301223b5425d537eb3ce8a18696edc1
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-07-31 18:26:10 +0530

    JS practice example

---
+++ JSExamples.js
@@ -0,0 +1,266 @@
+ 
+// let str = "Abraa-Ka-Daabraa"
+// // for(i=str.length-1;i>=0;i--)
+// // {
+// //     console.log(str[i]);
+// // }
+// int main() {
+//     char str[50];
+//     int i, l;
+//     int flag = 0;
+
+//     printf("Enter string: ");
+//     scanf("%s", str);
+//     l = strlen(str);
+
+//     for(i = 0; i < l / 2; i++) {
+//         if(str[i] != str[l-i-1]) {
+//             flag = 1;
+//             break;   }
+//     }
+//     if(flag) {
+//         printf("No. is not a palindrome.");
+//     } else {
+//         printf(" No. is a palindrome.");
+//     }
+// }
+
+
+// let n ; 
+// rev=0;
+// while(n!=0)
+// l= n%10
+// 	rev = rev *10 +l
+// n= n/10
+// if (rev== n)
+// {
+// 	print
+// 	("No. is palindrome ")
+// }
+// else{
+// 	print("NO. is not palindrome")
+// }
+
+//***********switch*********** */
+
+/*
+
+var trafficSignalCounter = 2
+
+switch(trafficSignalCounter)
+{
+	case 1:
+		console.log("Green")
+		break
+	case 2:
+		console.log("Yellow")
+		break
+	case 3:
+		console.log("Red")
+		break
+default:
+	console.log("Default")
+}
+
+*/
+//--------------------------------
+//***********IF-ELSE*********** */
+
+/*
+
+var trafficSignalCounter = 2
+
+if(trafficSignalCounter == 1)
+{
+	console.log("Green")
+}
+else if(trafficSignalCounter == 2)
+{
+	console.log("Yellow")
+}
+else if(trafficSignalCounter == 3)
+{
+	console.log("Red")
+}
+else
+{
+	console.log("Default")
+}
+
+*/
+
+//----------------------------------
+
+
+//**********	VARIABLES **********
+
+//---------LET & VAR (we can change)--------
+/*
+
+let var1=10     
+console.log(var1)
+
+var1=12              //number
+console.log(var1)
+
+
+var1="hello JS Variable" //String
+console.log(var1)
+
+
+var1='A'           //char     
+console.log(var1)
+
+Var1 = true
+console.log(var1)
+
+*/
+//-------------------------------
+
+//**************PRINT HELLO  *******
+
+// console.log("Hello")
+
+//--------------------------------------
+
+ 
+
+
+//****************CLASS***********
+/* 
+
+let a=5
+let b=4
+console.log(a)
+console.log(b)
+
+//a-=b
+console.log(a-=b)
+//-------------------------------------------
+ */
+
+// console.log("Addition : 5 + 3 = "+(5-3))
+
+//-------------------------------
+
+// var myStringVariable = "Abcdefghijkl"
+  
+// let myArrayVar = myStringVariable.split("f")
+// console.log(myArrayVar)
+
+//-------------------------------------------------
+
+
+
+//-----------------------------------------------------------
+
+/*
+
+for(var conditionCheck = 0; conditionCheck<=3; conditionCheck++)
+{ 
+	for(var tempconditionCheck = 0 ; tempconditionCheck<=conditionCheck; tempconditionCheck++)
+	{
+		//console.log("*") 
+		process. stdout. write("*")
+		 
+		 
+	}  console.log("\n") 
+}
+'
+ */
+//-----------------------------------------------------------------------
+
+// class Student{
+// 	name 
+// 	roll
+// 	constructor(name,roll){
+// 		this.name=name
+// 		this.roll=roll
+// 	}
+// }
+// let st =new Student("ssi",123)
+// console.log(st.name)
+
+//--------------------------------------------------------------------------------
+
+// let A = [
+//     {
+//         name: 'john',
+//         age: 12,
+//     },
+// 	{
+// 		city:
+// 		{
+// 			pin:'1234',
+// 		}
+		  
+// 	}
+// ];
+// console.log(A[1].city.pin);
+
+// --------------------------------------------------------
+
+
+
+// let A = [
+//     {
+//         name: 'john',
+//         age: 12,
+//     },
+// 	{
+// 		city:
+// 		{
+// 			pin:'1234',
+// 			location:
+// 			{
+// 			    indore:["mgroad","rau"],
+// 			}
+// 		}
+		 
+// 	}
+// ];
+// console.log(A[1].city.location.indore[1]);
+
+// ----------------------------------------------------------------
+
+// const A = [1, 4, 9, 16,1,2,4,67];
+// A.length=5
+// console.log(A);
+// console.log(A[5]);
+//------------------------------------------------------------------------
+
+
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 1, 'Feb');
+// // Inserts at index 1
+// console.log(months);
+// // Expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+// months.splice(4, 1, 'May');
+// // Replaces 1 element at index 4
+// console.log(months);
+// // Expected output: Array ["Jan", "Feb", "March", "April", "May"]
+
+// ----------------------------------------
+
+//  const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//  fruits.join(" * ");
+ 
+// // console.log(A)
+ 
+// console.log(fruits.length)
+
+//-------------------------------------------------------------------------------------
+
+
+// const age =[23,45,10,45,33,21,20,15,]
+// const ages= age.some((element)=>elemen>18)
+
+
+// --------------------------------------------------------------------------
+
+
+/**********************************- UDEMY - COURSE -************************************** */
+
+
+
